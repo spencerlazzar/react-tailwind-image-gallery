6cd92f0f11b34bf0f18c0b27a802d2fbafd1eb05
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-05-17 14:27:11 -0700

    Overall complete functionality

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,12 @@
-import React from 'react'
+  
+import React from 'react';
 
 const ImageCard = ({ image }) => {
-  const tags = image.tags.split(',')
-
+  const tags = image.tags.split(',');
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" className="w-full" />
+      <img src={image.webformatURL} alt="" className="w-full"/>
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by {image.user}
@@ -27,14 +27,14 @@ const ImageCard = ({ image }) => {
         </ul>
       </div>
       <div className="px-6 py-4">
-        {tags.map(tag => (
-          <span key={image.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #{tag}
-          </span>
+        {tags.map((tag, index) => (
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #{tag}
+        </span>
         ))}
       </div>
     </div>
   )
 }
 
-export default ImageCard
+export default ImageCard;
